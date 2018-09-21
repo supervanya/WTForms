d22a6d4826328576bb9ad15c2d98ae669ba9fceb
SI364-FA18 <42853806+SI364-FA18@users.noreply.github.com>
2018-09-21 12:10:33 -0400

    Add files via upload

---
+++ challenges.py
@@ -0,0 +1,26 @@
+#import statements go here 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.debug = True
+
+@app.route('/')
+def home():
+    return "Hello, world!"
+    
+#create class to represent WTForm that inherits flask form
+
+@app.route('/itunes-form')
+def itunes_form():
+    #what code goes here?
+    return render_template('itunes-form.html', form=simpleForm) # HINT : create itunes-form.html to represent the form defined in your class
+
+@app.route('/itunes-result', methods = ['GET', 'POST'])
+def itunes_result():
+    #what code goes here?
+    # HINT : create itunes-results.html to represent the results and return it
+    flash('All fields are required!')
+    return redirect(url_for('itunes_form')) #this redirects you to itunes_form if there are errors
+
+if __name__ == '__main__':
+    app.run()
